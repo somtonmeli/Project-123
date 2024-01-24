b850262152c7dd0d200e29d36b7e4d0cd58469c9
somtonmeli <115658287+somtonmeli@users.noreply.github.com>
2024-01-24 18:43:45 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,28 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(500,500)
+    canvas = createCanvas(500,500);
+    canvas.position(560,150);
+
+    video = createCapture(VIDEO);
+    video.size(500,550);
+    video.position(30,100);
+
+    posNet = ml5.posNet(video,modelLoaded);
+    posNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("The model has Loaded");
+}
+
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
 }
 
 function draw()
